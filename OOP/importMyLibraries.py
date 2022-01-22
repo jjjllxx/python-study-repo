07b61da53c7ef8c2465249ba0b727d2bd24e46e7
elif <60777462+JinLexuan@users.noreply.github.com>
2022-01-22 09:52:03 +0800

    Add files via upload

---
+++ OOP/importMyLibraries.py
@@ -0,0 +1,13 @@
+"""
+File: daorubao.py
+Author: Jin Lexuan
+E-mail: jlx321@126.com
+Time: 2020-02-23 15:15:05
+Function:
+
+
+"""
+
+import myLibraries
+myLibraries.send_messgae.send_message()
+myLibraries.receive_message.receive_message()
