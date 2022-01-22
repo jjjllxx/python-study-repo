ad306ba2d1330cdd62459402735581b69ea7c0f8
elif <60777462+JinLexuan@users.noreply.github.com>
2022-01-22 09:52:29 +0800

    Add files via upload

---
+++ OOP/myLibraries/receive_message.py
@@ -0,0 +1,13 @@
+"""
+File: receive_message.py
+Author: Jin Lexuan
+E-mail: jlx321@126.com
+Time: 2020-02-23 15:11:49
+Function:
+
+
+"""
+
+
+def receive_message():
+    print('this is a message from ')
---
+++ OOP/myLibraries/send_messgae.py
@@ -0,0 +1,13 @@
+"""
+File: send_messgae.py
+Author: Jin Lexuan
+E-mail: jlx321@126.com
+Time: 2020-02-23 15:11:37
+Function:
+
+
+"""
+
+
+def send_message():
+    print('send a message')
